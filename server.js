8fa1e16005f468576d237def3259883fae120564
Sunil Sharma <sunsharm6@publicisgroupe.net>
2019-11-19 09:51:34 +0530

    added host

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/'));
-app.listen(process.env.PORT || 5000);
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
